3a45e45bf5170afd8aa88b236d02ca7fc380e146
Erik Axel Eggeling <brannbartavfall@hotmail.com>
2017-11-30 22:53:54 +0100

    Updated metadata

--- options.js
+++ options.js
@@ -1,29 +1,22 @@
-// Saves options to chrome.storage.sync.
-function save_options() {
+function saveOptions() {
   var timeValue = document.getElementById('countdown').value;
   chrome.storage.sync.set({
     countdown: timeValue
   }, function() {
-    // Update status to let user know options were saved.
     var status = document.getElementById('status');
     status.textContent = 'Countdown time saved.';
-    console.log("saving");
     setTimeout(function() {
       status.textContent = '';
     }, 2000);
   });
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-  // Use default value timeValue = 'red' and likesColor = true.
+function restoreOptions() {
   chrome.storage.sync.get({
     countdown: 6,
   }, function(items) {
     document.getElementById('countdown').value = items.countdown;
   });
 }
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.getElementById('save').addEventListener('click', saveOptions);
